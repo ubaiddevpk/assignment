3392b25c26715b9ed9e198880077bfc84902ac0b
ubaiddevpk <168396695+ubaiddevpk@users.noreply.github.com>
2024-06-17 00:06:32 +0500

    Add files via upload

---
+++ age_eligibility.js
@@ -0,0 +1,9 @@
+let age = prompt("Enter your age ")
+if(age>=18&&age<=65)
+    {
+        alert("you are eligible to cast VOTE")
+    }
+    else
+    {
+        alert("Yor are age is " + age + " " + " . You are not eligible to cast VOTE")
+    }
