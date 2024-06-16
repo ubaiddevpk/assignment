281aceedd81b1b1da4ccd3c1958f4275a7f9c557
ubaiddevpk <168396695+ubaiddevpk@users.noreply.github.com>
2024-06-17 00:18:13 +0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+let x;
+let y;
+x=prompt("Enter your user name : ")
+y=prompt("Enter your user passward : ")
+if(x==="obaid"||y==="khan")
+    {
+        alert("login succesful ")
+    }
+    else
+    {
+    alert("invilid user name and passward")
+    }
